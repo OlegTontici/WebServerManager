0a6d0b324030572f3a62b0861ee075ae2d000a50
Tontici Oleg <oleg.tontici@jivygroup.com>
2018-02-22 09:56:59 +0200

    add abillity to open phisycal path for an application

--- src/IISManager/ApplicationManager.xaml.cs
+++ src/IISManager/ApplicationManager.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace IISManager
 {
@@ -10,6 +11,12 @@ namespace IISManager
         public ApplicationManager()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
         }
     }
 }
